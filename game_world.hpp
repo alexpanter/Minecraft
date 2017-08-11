b9fd0acd08e6720b3d58136806007d9f703d4f30
Alexander Christensen <alex_c007@hotmail.com>
2017-08-11 18:56:22 +0200

    Complete redesign of game world construction

---
+++ game_world.hpp
@@ -0,0 +1,188 @@
+// GLEW
+#ifndef GLEW_STATIC
+#define GLEW_STATIC
+#endif
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+//#include <glm/gtc/constants.hpp>
+
+// STANDARD
+#include <iostream> // std::cerr
+
+
+typedef enum {
+    BLOCK_TYPE_EARTH,
+    BLOCK_TYPE_GRASS,
+    BLOCK_TYPE_STONE,
+
+    // a block that does not exist, should not be used
+    // for collision detection or AI routines, and
+    // definitely not should be drawn.
+    BLOCK_TYPE_NONE
+} _block_type_t;
+
+typedef struct _block_t {
+    // default health of a block is 10. When health
+    // reaches < 0 it should be destroyed.
+    _block_t() : type(BLOCK_TYPE_NONE), health(0) {}
+    _block_t(_block_type_t type) : type(type), health(10) {}
+    _block_t(_block_type_t type, int health) : type(type), health(health) {}
+
+    _block_type_t type;
+    int health;
+} _block_t;
+
+
+class GameWorld
+{
+    // dimensions of the game world
+    int _width;
+    int _height;
+    int _depth;
+
+    _block_t* _blocks;
+
+    // default vertex buffer data to satisfy OpenGL, for now..
+    GLuint _VBO, _VAO;
+
+    GLfloat _vertices[3] = {
+        // Positions
+        0.0f, 0.0f, 0.0f
+    };
+
+    void BufferVertexData()
+    {
+        glGenVertexArrays(1, &_VAO);
+        glGenBuffers(1, &_VBO);
+
+        // bind the VAO first, then bind and set vertex buffer(s)
+        // and attribute pointer(s)
+        glBindVertexArray(_VAO);
+
+        // vertices
+        glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(_vertices), _vertices, GL_STATIC_DRAW);
+
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);
+
+        // good practice to unbind the VAO to prevent strange bugs
+        // do NOT unbind the EBO, keep it bound to this VAO
+        glBindVertexArray(0);
+    }
+
+    inline int get_array_position(int x, int y, int z)
+    {
+        return (z * _height * _width) + (y * _height) + x;
+    }
+
+
+public:
+    GameWorld(int width, int height, int depth)
+        : _width(width), _height(height), _depth(depth)
+    {
+        // after this initialization, all blocks will have 10 health
+        // and marked as `BLOCK_TYPE_NONE'.
+        _blocks = new _block_t[_height * _width * _depth];
+
+        BufferVertexData();
+    }
+
+    ~GameWorld()
+    {
+        delete _blocks;
+        glDeleteVertexArrays(1, &_VAO);
+        glDeleteBuffers(1, &_VBO);
+    }
+
+    // return false if there is already a block at the desired entry
+    //
+    // TODO: block health might be set automatically within this method
+    // according to block type
+    bool InsertBlock(int x, int y, int z, _block_type_t type, int health = 10)
+    {
+        if(health < 1)
+        {
+            std::cerr << "Inserting block with health < 1" << std::endl;
+            health = 10;
+        }
+
+        int index = get_array_position(x, y, z);
+        if(_blocks[index].type != BLOCK_TYPE_NONE)
+        {
+            return false;
+        }
+
+        _blocks[index] = _block_t(type, health);
+        return true;
+    }
+
+    bool DeleteBlock(int x, int y, int z)
+    {
+        int index = get_array_position(x, y, z);
+        if(_blocks[index].type != BLOCK_TYPE_NONE)
+        {
+            return false;
+        }
+
+        // more explicit, could use constructor with empty argument list as well
+        _blocks[index] = _block_t(BLOCK_TYPE_NONE, 0);
+        return true;
+    }
+
+    // TODO: return something player can add to inventory..
+    void DecreaseBlockHealth(int x, int y, int z, int health_decrease)
+    {
+        if(health_decrease < 0)
+        {
+            std::cerr << "Decreasing block health with a negative amount!"
+                      << std::endl;
+            health_decrease = 0;
+        }
+        int index = get_array_position(x, y, z);
+        _blocks[index].health -= health_decrease;
+
+        if(_blocks[index].health < 0)
+        {
+            _blocks[index].type = BLOCK_TYPE_NONE;
+        }
+    }
+
+    // VERY naive approach, but good enough for simple demonstration
+    void DrawBlocks(GLuint shader, int size)
+    {
+        glBindVertexArray(_VAO);
+        GLint model_loc = glGetUniformLocation(shader, "model");
+
+        for(int i = 0; i < _width; i++)
+        {
+            for(int j = 0; j < _height; j++)
+            {
+                for(int k = 0; k < _width; k++)
+                {
+                    if(_blocks[get_array_position(i, j, k)].type != BLOCK_TYPE_NONE)
+                    {
+                        glm::mat4 model;
+                        model = glm::translate(model, glm::vec3(i*size,
+                                                                j*size,
+                                                                k*size));
+                        glUniformMatrix4fv(model_loc, 1, GL_FALSE, glm::value_ptr(model));
+
+                        glDrawArrays(GL_POINTS, 0, 1);
+                    }
+
+                }
+            }
+        }
+
+        glBindVertexArray(0);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@
 #include "engine/window.hpp"
 #include "engine/texture.hpp"
 #include "engine/camera.hpp"
+#include "game_world.hpp"
 
 
 
@@ -29,31 +30,32 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void do_movement(GLfloat deltaTime);
 
 // VARIABLES
-GLfloat size = 10.0f;
 camera::BasicFPSCamera* fps_cam;
 
-// VERTEX DATA
-GLfloat vertices[] = {
-    // Positions
-    0.0f, 0.0f, 0.0f
-};
-glm::vec3 blockPositions[] = {
-    glm::vec3( 0.0f, 0.0f, -size),
-    glm::vec3(-size, 0.0f, -size),
-    glm::vec3( size, 0.0f, -size),
-    glm::vec3( 0.0f, 0.0f,  0.0f),
-    glm::vec3(-size, 0.0f,  0.0f),
-    glm::vec3( size, 0.0f,  0.0f),
-    glm::vec3( 0.0f, 0.0f,  size),
-    glm::vec3(-size, 0.0f,  size),
-    glm::vec3( size, 0.0f,  size),
+// GAME WORLD
+//GameWorld* game_world;
+int block_size = 10;
 
-    glm::vec3(-size, size, -size),
-    glm::vec3( 0.0f, size, -size),
-    glm::vec3(-size, size,  0.0f),
+void create_world(GameWorld* world)
+{
+    world->InsertBlock(0, 0, 0, BLOCK_TYPE_GRASS);
+    world->InsertBlock(1, 0, 0, BLOCK_TYPE_GRASS);
+    world->InsertBlock(2, 0, 0, BLOCK_TYPE_GRASS);
 
-    glm::vec3(-size, 2*size, -size)
-};
+    world->InsertBlock(0, 0, 1, BLOCK_TYPE_GRASS);
+    world->InsertBlock(1, 0, 1, BLOCK_TYPE_GRASS);
+    world->InsertBlock(2, 0, 1, BLOCK_TYPE_GRASS);
+
+    world->InsertBlock(0, 0, 2, BLOCK_TYPE_GRASS);
+    world->InsertBlock(1, 0, 2, BLOCK_TYPE_GRASS);
+    world->InsertBlock(2, 0, 2, BLOCK_TYPE_GRASS);
+
+    world->InsertBlock(2, 1, 1, BLOCK_TYPE_GRASS);
+    world->InsertBlock(1, 1, 2, BLOCK_TYPE_GRASS);
+    world->InsertBlock(2, 1, 2, BLOCK_TYPE_GRASS);
+
+    world->InsertBlock(2, 2, 2, BLOCK_TYPE_GRASS);
+}
 
 // activated keys
 bool keys[512]; // perhaps 512 is not sufficient for some keyboards
@@ -66,11 +68,18 @@ int main()
     window::as_ratio_t as_ratio = window::ASPECT_RATIO_4_3;
     window::WindowedWindow* win = window::create_window(title, 800, as_ratio);
 
+    // GAME WORLD
+    GameWorld* game_world = new GameWorld(3, 3, 3);
+
+    create_world(game_world);
+
     // KEY EVENTS
     glfwSetKeyCallback(win->Window(), key_callback);
 
     // CAMERA
     fps_cam = new camera::BasicFPSCamera(win->Window(), win->width, win->height);
+    fps_cam->SetInitialPosition(0.0f, block_size * 1.0f, block_size * 1.0f);
+    //fps_cam->SetInitialDirection(0.0f, 0.0f, 0.0f);
 
     // CURSOR
     glfwSetCursorPosCallback(win->Window(), mouse_callback);
@@ -85,29 +94,7 @@ int main()
 
     // TEXTURES
     unsigned long tex_options = TEX_GENERATE_MIPMAP | TEX_MIXED_FILTER;
-    Texture tex0 = Texture("assets|images|block_dirt_1.png", tex_options);
-
-    // BUFFER OBJECTS
-    GLuint VBO, VAO;
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    // bind the VAO first, then bind and set vertex buffer(s)
-    // and attribute pointer(s)
-    glBindVertexArray(VAO);
-
-    // vertices
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    // good practice to unbind the VAO to prevent strange bugs
-    // do NOT unbind the EBO, keep it bound to this VAO
-    glBindVertexArray(0);
+    Texture tex0 = Texture("assets|images|block_grass_1.png", tex_options);
 
     // enable depth testing, by using the GLFW's z-buffer
     glEnable(GL_DEPTH_TEST);
@@ -143,7 +130,6 @@ int main()
         fps_cam->CalculatePosition();
 
         // model/view/projection uniform matrices
-        GLint model_loc = glGetUniformLocation(shader, "model");
         GLint view_loc = glGetUniformLocation(shader, "view");
         GLint projection_loc = glGetUniformLocation(shader, "projection");
         glUniformMatrix4fv(view_loc, 1, GL_FALSE,
@@ -157,18 +143,10 @@ int main()
         glUniform1i(glGetUniformLocation(shader, "texture0"), 0);
 
         // uniform block size
-        glUniform1f(glGetUniformLocation(shader, "sz"), size / 2.0f);
+        glUniform1f(glGetUniformLocation(shader, "sz"), (GLfloat)block_size / 2.0f);
 
         // drawing calls
-        glBindVertexArray(VAO);
-        for(GLuint i = 0; i < 13; i++) {
-            glm::mat4 model;
-            model = glm::translate(model, blockPositions[i]);
-            glUniformMatrix4fv(model_loc, 1, GL_FALSE, glm::value_ptr(model));
-
-            glDrawArrays(GL_POINTS, 0, 1);
-        }
-        glBindVertexArray(0);
+        game_world->DrawBlocks(shader, block_size);
 
         // double-buffering
         glfwSwapBuffers(win->Window());
@@ -176,8 +154,7 @@ int main()
 
     // do proper cleanup of any allocated resources
     delete(win);
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    delete game_world;
     glfwTerminate();
 
     // exiting the application
